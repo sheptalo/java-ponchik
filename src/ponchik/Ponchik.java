dfb133b551bc58d5d84dc2c8cd94bf4f8df0a4dc
solovovartem99@gmail.com <solovovartem99@gmail.com>
2022-10-16 14:32:45 +0300

    b

--- src/ponchik/Ponchik.java
+++ src/ponchik/Ponchik.java
@@ -1,5 +1,18 @@
-package ponchik.Ponchic;
+package ponchik;
 
+import arc.*;
+import arc.func.*;
+import arc.struct.*;
+import arc.util.*;
+import mindustry.*;
+import mindustry.game.EventType.*;
+import mindustry.game.*;
+import mindustry.mod.*;
+import mindustry.mod.Mods.*;
+import mindustry.net.*;
+import mindustry.type.*;
+import mindustry.world.*;
+import mindustry.world.blocks.*;
 import mindustry.mod.Mod;
 import ponchik.content.*;
 public class Ponchik extends Mod{
