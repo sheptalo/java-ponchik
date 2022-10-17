0cc2949679374a6c6f967b2491b275482c3bf534
solovovartem99@gmail.com <solovovartem99@gmail.com>
2022-10-17 16:40:06 +0300

    l

---
+++ src/ponchik/content/PoPlanets.java
@@ -0,0 +1,52 @@
+package mindustry.content;
+
+import arc.func.*;
+import arc.graphics.*;
+import arc.math.*;
+import arc.math.geom.*;
+import arc.struct.*;
+import arc.util.*;
+import mindustry.game.*;
+import mindustry.graphics.*;
+import mindustry.graphics.g3d.*;
+import mindustry.graphics.g3d.PlanetGrid.*;
+import mindustry.maps.planet.*;
+import mindustry.type.*;
+import mindustry.world.*;
+import mindustry.world.meta.*;
+
+
+
+public class PoPlanets{
+    public static Planet
+    Voulis
+
+
+
+public static void load(){
+        Voulis = new Planet("voulis", sun, 3f, 2){{
+            generator = new SerpuloPlanetGenerator();
+                meshLoader = () -> new HexMesh(this, 5);
+
+            launchCapacityMultiplier = 0.5f;
+            sectorSeed = 1;
+            allowWaves = true;
+            allowWaveSimulation = true;
+            allowSectorInvasion = true;
+            allowLaunchSchematics = true;
+            enemyCoreSpawnReplace = true;
+            prebuildBase = false;
+            ruleSetter = r -> {
+                r.waveTeam = Team.crux;
+                r.placeRangeCheck = false;
+            };
+            iconColor = Color.valueOf("7d4dff");
+            atmosphereColor = Color.valueOf("3c1b8f");
+            atmosphereRadIn = 0.02f;
+            atmosphereRadOut = 0.3f;
+            startSector = 15;
+            alwaysUnlocked = true;
+            hiddenItems.addAll(Items.erekirItems).removeAll(Items.serpuloItems);
+        }};
+} 
+} 
